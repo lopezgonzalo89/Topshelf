608daadf677bca5e307715d1b474cbdc8d22dc53
lopezgonzalo89@ <lopezgonzalo.sm89@gmail.com>
2019-10-09 01:43:37 -0300

    Andando con tablas suplentes en DB Local

--- CheckVentas/Clases/MyService.cs
+++ CheckVentas/Clases/MyService.cs
@@ -9,32 +9,102 @@ namespace CheckVentas.Clases
 {
     class MyService
     {
+        int ultVentaInt;
+
         public void Start()
-        {
-            // Conectarme a la base de 
+        {            
             Console.WriteLine("En el Start");
-            String datos = "";
             try
             {
-                MySqlConnection conexion = new MySqlConnection("Server=localhost;Database=choppin_db;Uid=root;Pwd=;");
-                conexion.Open();
-                Console.WriteLine("Conectadooo");
-                MySqlDataReader reader = null;
-                MySqlCommand cmd = new MySqlCommand("select * from productoventa", conexion);
-                reader = cmd.ExecuteReader();
+                // Abro Con 0
+                MySqlConnection conexion0 = new MySqlConnection("Server=localhost;Database=choppin_db;Uid=root;Pwd=;");
+                conexion0.Open();                
 
-                while (reader.Read())
-                {
-                    datos += reader.GetString("IdVenta") + "\n";
+                // Sql cmd 0 -> Trae tabla ultventa
+                MySqlCommand cmd0 = new MySqlCommand("select * from ultventa order by ultventa desc limit 1;", conexion0);
+                MySqlDataReader reader0 = cmd0.ExecuteReader();
+
+                while (reader0.Read()) { 
+                    String UltVentaString = reader0.GetString("UltVenta");
+                    ultVentaInt = int.Parse(UltVentaString);
                 }
+                // Cierro con 0
+                conexion0.Close();
+
+                // Abro con 1
+                MySqlConnection conexion1 = new MySqlConnection("Server=localhost;Database=choppin_db;Uid=root;Pwd=;");
+                conexion1.Open();                 
+
+                // Sql cmd 1 -> Trae todas las ventas
+                MySqlCommand cmd1 = new MySqlCommand("select * from ventas", conexion1);
+                MySqlDataReader reader1 = cmd1.ExecuteReader();
+
+                while (reader1.Read())
+                {
+                    String IdVentaString = reader1.GetString("IdVenta");
+
+                    int IdVentaInt = int.Parse(IdVentaString);
+                    if (ultVentaInt <= IdVentaInt)
+                    {
+                        // Abro Con 3
+                        MySqlConnection conexion3 = new MySqlConnection("Server=localhost;Database=choppin_db;Uid=root;Pwd=;");
+                        conexion3.Open();
+
+                        // Sql cmd 3 -> Actualizo ultVenta con el IdVenta
+                        MySqlCommand cmd4 = new MySqlCommand("INSERT INTO `ultventa` (`UltVenta`) VALUES ('"+ IdVentaInt +"');", conexion3);
+                        MySqlDataReader reader4 = cmd4.ExecuteReader();
+                        conexion3.Close();
+
+                        Console.WriteLine("UltVentaInt: " + ultVentaInt);
+                        Console.WriteLine("IdVentaInt: " + IdVentaInt);
+                        //obtener IdProductoVenta y cantidad del IdVenta
+                        String IdProductoVenta = reader1.GetString("IdProductoVenta");
+                        String CantidadVenta = reader1.GetString("Cantidad");
+                        int CantVenta = int.Parse(CantidadVenta);
+
+                        // Vuelvo a abrir con 0
+                        conexion0.Open();
+
+                        // cmd 2 -> trae la receta segun el IdProductoVenta                        
+                        MySqlCommand cmd2 = new MySqlCommand("Select * from recetas where IdProductoVenta = " + IdProductoVenta + "", conexion0);
+                        MySqlDataReader read = cmd2.ExecuteReader();
+                        while (read.Read())
+                        {
+                            String Cantidad = read.GetString("Cantidad");
+                            int Cant = int.Parse(Cantidad);
+                            DateTime hoy = DateTime.Now;
+
+                            // Defino Fecha, IdProducto, CantTotal, IdTipoMov, Nota
+                            string fecha = hoy.ToString("yyyy/MM/dd");
+                            String IdProducto = read.GetString("IdProducto");
+                            int CantTotal = Cant * CantVenta;
+                            String idTipoMov = "3";
+                            String nota = "Insertado desde C#";
+
+                            Console.WriteLine("fecha: " + fecha);
+                            Console.WriteLine("IdProducto: " + IdProducto);
+                            Console.WriteLine("Cantidad: " + CantTotal);
+
+                            // Abro con 2
+                            MySqlConnection conexion2 = new MySqlConnection("Server=localhost;Database=choppin_db;Uid=root;Pwd=;");
+                            conexion2.Open();
+
+                            MySqlCommand cmd3 = new MySqlCommand("INSERT INTO `movimientos` (`IdMovimiento`, `IdProducto`, `Fecha`, `Cantidad`, `Nota`, `IdTipoMovimiento`) VALUES (NULL, '" + IdProducto + "', '" + fecha + "', '-" + CantTotal + "', '" + nota + "', '" + idTipoMov + "')", conexion2);
+                            MySqlDataReader dr = cmd3.ExecuteReader();
+                            conexion2.Close();
+
+                        }
+                        conexion0.Close();
+                    }
+                }
+                conexion1.Close();
             }
+                // Volver a Comparar en 15'
+            
             catch (MySqlException e)
             {
                 Console.WriteLine("MySqlException: " + e);
             }
-
-            Console.WriteLine(datos);
-
         }
         public void Stop()
         {
--- CheckVentas/Program.cs
+++ CheckVentas/Program.cs
@@ -3,15 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CheckVentas
 {
     class Program
     {
-        static void Main(string[] args)
-        {         
-          ConfigureService.Configure();         
+        
+        static void Main()
+        {
+            Timer tmr = new Timer(Tick, "tick...", 5000, 1000);
+            Console.ReadLine();
+            // Libera todos los recursos utilizados por la instancia actual de System.Threading.Timer.
+            tmr.Dispose();
+        }
+        static void Tick(object data)
+        {
+            ConfigureService.Configure();
+            // Esto se ejecuta en un hilo combinado
+            Console.WriteLine(data); // Escribe "tick..."
         }
     }
 }
